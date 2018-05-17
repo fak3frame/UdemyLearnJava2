3beb9688415f4e193ece0d5f145cb606d1842d4c
fak3frame <kamil7745@gmail.com>
2018-05-17 16:30:01 +0200

    poprawki 13

--- Udemy/src/Kolekcje_ude/K3_Wildcards/Wildcards.java
+++ Udemy/src/Kolekcje_ude/K3_Wildcards/Wildcards.java
@@ -47,26 +47,33 @@ public class Wildcards {
         listaClass.add(new SuperClass());
         listaClass.add(new SubClass());//moge dodac takze podklase poniewaz dziedziczy
 
-        SuperClass.tester(listaStringow); //zadziala bo przyjmuje liste dowolnego typu klasy Object
-        //SuperClass.tester2(listaStringow);//nie zadziala bo metoda przyjmuje tablice z obiektami
-        // z rodziny klasy Object oraz nie specyficznej jak "Superclass"
+        SuperClass.tester(listaStringow); //zadziala bo przyjmuje liste dowolnego typu
+        SuperClass.tester(listaClass);
 
-        //ArrayList<String> listaS = new ArrayList<String>();
-        //ArrayList<Object> listaO = new ArrayList<Object>(); - moge tutaj dodac wszystko (integer/string)
+        ArrayList<Object> o = new ArrayList();
+        SuperClass.tester2(o);
+        //SuperClass.tester2(listaStringow);//nie zadziala bo metoda przyjmuje tablice z obiektami
+        //SuperClass.tester2(listaClass);
+        //nie zadziala takze poniewaz lista listaClass nie jest powiazana z klasa Object (nie rozszerza jej)
+
+        ArrayList<String> listaS = new ArrayList<String>();
+        ArrayList<Object> listaO = new ArrayList<Object>();// - moge tutaj dodac wszystko (integer/string)
+
         SuperClass.testerWildcards(listaStringow); //przyjmuje wszystko
         SuperClass.testerWildcards(listaClass);
+        SuperClass.testerWildcards(o);
         //zadzialaja obie bo metoda testerWildcards przyjmuje ArrayList dowolnego typu
-        //z obiketami lub zmiennymi typu String/Integer
+        //z obiketami mojej klasy lub typu String/Integer lub klasy Object
 
         ArrayList<SubClass> listaSubClass = new ArrayList<SubClass>();
+
+        SuperClass.testerWIldcards2(listaClass);
+        SuperClass.testerWIldcards2(listaSubClass);
         //SuperClass.testerWIldcards2(listaStringow);
         //nie zadziala bo przyjmuje tylko elementy tablicy obiektow superklasy lub klas dziedziczacych
         //(ArrayList<? extends SuperClass> lista)
 
         //? - znak wieloznacznosci Wildcards
         //? extends jakasKlasa - znak wieloznacznosci ograniczony jakasKlasa - bounded Wildcards
-
-        SuperClass.testerWIldcards2(listaClass);
-        SuperClass.testerWIldcards2(listaSubClass);
     }
 }
